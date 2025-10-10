5d5d86de1fa0e919ef10e275336c4ad36a63de42
NawarSo <sbbnawwar@gmail.com>
2025-10-10 22:05:02 +0300

    convert the api to version 1 of the api

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const planetsRouter = require('./routes/planets/planets.Router');
-const launchesRouter = require('./routes/launches/launches.router');
 const cors = require('cors');
 const path = require("path");
 const morgan = require('morgan');
 const app = express();
-
+const v1 = require('./routes/v1');
 app.use(cors({
     origin:'http://localhost:3000',
 }));
@@ -14,10 +12,9 @@ app.use(morgan('combined'));
 
 app.use(express.json()); // to parse any JSON data come from requests'
 app.use(express.static(path.join(__dirname,"..","public "))); // note there is space after the name public
-app.use(planetsRouter); 
-app.use('/launches',launchesRouter);
+app.use('/v1',v1); // contain the launches & planets routes
 app.use('/',(req,res) => {
     res.sendFile(path.join(__dirname,"..","public ","index.html"));
-})
+});
 
 module.exports = app;
